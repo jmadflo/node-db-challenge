4a947b2d6c777a558c3809b0c2e37d250c343e13
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-05-15 10:26:20 -0700

    tasks fixed

--- api/tasks/tasksModel.js
+++ api/tasks/tasksModel.js
@@ -5,9 +5,11 @@ module.exports = {
     addTask
 }
 
-// gets all tasks
+// gets all tasks and show project name and product description
 function getTasks(){
-    return db('tasks')
+    return db.select('projects.name as project_name', 'projects.description as project_description', 'tasks.description as tasks_description', 'tasks.notes', 'tasks.completed')
+        .from('tasks')
+        .join('projects', 'tasks.project_id', 'projects.id')
 }
 
 // getstask by id
